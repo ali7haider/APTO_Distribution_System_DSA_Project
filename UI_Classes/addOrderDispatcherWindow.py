64457b726096b4f554f5a320b3433d28c90acbae
Ali Haider <haider556871@gmail.com>
2024-02-17 18:47:05 +0500

    Edit Order Dispatcher Window Added

--- UI_Classes/addOrderDispatcherWindow.py
+++ UI_Classes/addOrderDispatcherWindow.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Feb 17 18:41:58 2024
+Created on Sat Feb 17 18:44:30 2024
 
 @author: Digital Zone
 """
@@ -14,32 +14,15 @@ from datetime import date
 from PyQt5.QtGui import QIntValidator
 from PyQt5 import QtCore, QtGui
 from datetime import datetime
-class EditOrderDispatcher(QMainWindow,Ui_MainWindow):
-    def __init__(self,S):
-        super(EditOrderDispatcher,self).__init__()
+class AddOrderDispatcherWindow(QMainWindow,Ui_MainWindow):
+    def __init__(self):
+        super(AddOrderDispatcherWindow,self).__init__()
         self.setupUi(self)
         self.file_paths = FilePaths(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'Data')))      
-        self.txtID.setEnabled(False)
-        self.implementingValidation()  
-        self.fillInformation(S)
-        
-        # self.btnAdd.clicked.connect(self.saveNewSaleAgentData)
-        self.btnSave.clicked.connect(lambda:self.saveODData(S))
+        self.btnAdd.clicked.connect(self.saveNewOrderDispatcherData)
         self.btnCancel.clicked.connect(lambda: self.close())
-    
-    def fillInformation(self,S):
-        self.txtID.setEnabled(False)
-        self.txtID.setText(S.Id)
-        self.txtName.setText(S.name)
-        self.txtCNIC.setText(S.cnic)
-        self.txtEmail.setText(S.email)
-        self.txtUserName.setText(S.login.userName)
-        self.txtPassword.setText(S.login.password)
-        self.txtCellNo.setText(S.cellNo)
-        self.txtSalary.setText(S.salary)
-        expiryDate = datetime.strptime(S.dateCreated, '20%y-%m-%d').date()
-        self.dateEditAddAgent_2.setDate(expiryDate)
-        
+        self.implementingValidation()
+        self.dateAddOrderDispatcher.setDate(date.today())
     def implementingValidation(self):
         rx  = QtCore.QRegExp("[0-9]{13}")   
         val = QtGui.QRegExpValidator(rx)
@@ -48,6 +31,7 @@ class EditOrderDispatcher(QMainWindow,Ui_MainWindow):
         self.txtSalary.setValidator(QIntValidator())
         self.txtCellNo.setInputMask("+99_999_9999999")
         self.txtID.setInputMask("O_999999")
+            
     def ValidationChecker(self,Id,name,cnic,email,userName,password,cellNo,salary,dateCreated):
         flag=True
         if Id=="O_":            
@@ -56,7 +40,7 @@ class EditOrderDispatcher(QMainWindow,Ui_MainWindow):
         else:
             self.lblIDValidation.setText("")        
         if name=="":
-            self.lblNameValidation.setText("*Enter Name")
+            self.lblNameValidation.setText("*Enter Name ")
             flag=False
         else:
             self.lblNameValidation.setText("")
@@ -89,35 +73,39 @@ class EditOrderDispatcher(QMainWindow,Ui_MainWindow):
             flag=False
         else:
             self.lblSalaryValidation.setText("")
+           
         today=date.today()
         dateToday=today.strftime("20%y-%m-%d")
-        if dateCreated<dateToday:
-            self.lblDateValidation_2.setText("*Date should be of Today or Greater")
-            flag=False
-        else:
-            self.lblDateValidation_2.setText("")
+        # if dateCreated<dateToday:
+        #     self.lblDateValidation.setText("*Date should be of Today or Greater")
+        #     flag=False
+        # else:
+        #     self.lblDateValidation.setText("")
         if flag==True:
             return True
         else:
             return False
-    def saveODData(self,previous):
+    def saveNewOrderDispatcherData(self):
         Id = str(self.txtID.text())
         name = str(self.txtName.text())
-        cnic = self.txtCNIC.text()
+        cnic = str(self.txtCNIC.text())
         email = str(self.txtEmail.text())
         userName = str(self.txtUserName.text())
         password = str(self.txtPassword.text())
         cellNo = str(self.txtCellNo.text())
-        dateCreated = self.dateEditAddAgent_2.date().toPyDate()
+        today=date.today()
+        dateCreated = self.dateAddOrderDispatcher.date().toPyDate()
         role="Order Dispatcher"
         salary = self.txtSalary.text()
         if self.ValidationChecker(Id,name,cnic,email,userName,password,cellNo,salary,dateCreated):
-            new = orderDispatcher(userName, password,role, name, cnic, email, cellNo, Id, dateCreated,salary)
-            orderDispatcherDL().EditODDataToList(previous,new)
-            # salesManDL().addSalesmanToFile(salesAgent)
+            OD = orderDispatcher(userName, password,role, name, cnic, email, cellNo, Id, dateCreated,salary)
+            # print(salesAgent.Id,salesAgent.login.userName,salesAgent.login.password,salesAgent.login.role,salesAgent.cnic,salesAgent.email,salesAgent.cellNo,salesAgent.dateCreated,salesAgent.vehicle,salesAgent.salary)
+            # print(salesAgent.login.password)
+            orderDispatcherDL.addODMan(OD)
+            orderDispatcherDL().addODToFile(self.file_paths.OrderDispatcherInfo,OD)
             msg = QMessageBox()
             msg.setText("Done")
-            msg.setInformativeText('User Data Updated Succesfully')
+            msg.setInformativeText('User Added Succesfully')
             msg.setWindowTitle("Successful")
             msg.exec_()
             self.close()
